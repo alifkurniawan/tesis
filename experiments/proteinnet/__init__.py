45ddee3e8ca98028153989fdcdc3197b6bf16997
alifkurniawan <cs.alif.kurniawan@gmail.com>
2020-04-25 15:54:50 +0700

    update model

--- experiments/proteinnet/__init__.py
+++ experiments/proteinnet/__init__.py
@@ -10,6 +10,8 @@ from models import *
 from training import train_model
 from util import contruct_dataloader_from_disk
 
+data_paths = ['30', '50', '70', '90', '95', '100']
+
 
 def run_experiment(parser, use_gpu):
     # parse experiment specific command line arguments
@@ -20,24 +22,25 @@ def run_experiment(parser, use_gpu):
     # pre-process data
     process_raw_data(use_gpu, force_pre_processing_overwrite=True)
 
-    # run experiment
-    training_file = "data/preprocessed/training_30.hdf5"
-    validation_file = "data/preprocessed/validation.hdf5"
+    for path in data_paths:
+        # run experiment
+        training_file = "data/preprocessed/training_" + path + "'.hdf5"
+        validation_file = "data/preprocessed/validation.hdf5"
 
-    model = ExampleModel(21, args.minibatch_size, use_gpu=use_gpu)  # embed size = 21
+        model = ExampleModel(21, args.minibatch_size, use_gpu=use_gpu)  # embed size = 21
 
-    train_loader = contruct_dataloader_from_disk(training_file, args.minibatch_size)
-    validation_loader = contruct_dataloader_from_disk(validation_file, args.minibatch_size)
+        train_loader = contruct_dataloader_from_disk(training_file, args.minibatch_size)
+        validation_loader = contruct_dataloader_from_disk(validation_file, args.minibatch_size)
 
-    train_model_path = train_model(data_set_identifier="TRAIN",
-                                   model=model,
-                                   train_loader=train_loader,
-                                   validation_loader=validation_loader,
-                                   learning_rate=args.learning_rate,
-                                   minibatch_size=args.minibatch_size,
-                                   eval_interval=args.eval_interval,
-                                   hide_ui=args.hide_ui,
-                                   use_gpu=use_gpu,
-                                   minimum_updates=args.minimum_updates)
+        train_model_path = train_model(data_set_identifier="TRAIN_" + path + "_150",
+                                       model=model,
+                                       train_loader=train_loader,
+                                       validation_loader=validation_loader,
+                                       learning_rate=args.learning_rate,
+                                       minibatch_size=args.minibatch_size,
+                                       eval_interval=args.eval_interval,
+                                       hide_ui=args.hide_ui,
+                                       use_gpu=use_gpu,
+                                       minimum_updates=args.minimum_updates)
 
-    print(train_model_path)
+        print(train_model_path)
