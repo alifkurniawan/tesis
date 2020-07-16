dc1cfacbd262887eb972bd5815bb458052a5b8b4
alifkurniawan <cs.alif.kurniawan@gmail.com>
2020-07-17 03:29:04 +0700

    combine all script terus tinggal tidur

--- run2.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python __main__.py --experiment-id proteinnet --minibatch-size 2 --use-gpu True --hide-ui --skenario 2
--- run8.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=7 python __main__.py --experiment-id proteinnet --minibatch-size 2 --use-gpu True --hide-ui --skenario 8
