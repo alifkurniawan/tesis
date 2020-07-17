1be529bb2f2830beb52f09dec8f652969808d548
alif <cs.alif.kurniawan@gmail.com>
2020-07-17 03:58:10 +0000

    commit

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 python __main__.py --experiment-id proteinnet --minibatch-size 32 --use-gpu True --hide-ui --skenario 3
+CUDA_VISIBLE_DEVICES=3 python __main__.py --experiment-id proteinnet --minibatch-size 128 --use-gpu True --hide-ui --skenario 3
