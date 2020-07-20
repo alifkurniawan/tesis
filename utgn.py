dbf503fe2d708c0d5cdb006eea3792ab90db9a93
alifkurniawan <cs.alif.kurniawan@gmail.com>
2020-07-20 12:00:21 +0700

    bug fixing again

--- utgn.py
+++ utgn.py
@@ -64,7 +64,8 @@ class UTGN(openprotein.BaseModel):
         encoder_layers = TransformerEncoderLayer(num_vocab, n_head, n_hid, dropout)
 
         # self.transformer_encoder = TransformerEncoder(encoder_layers, n_layers)
-        self.transformer_encoder = UniversalTransformer(encoder_layers, n_layers)
+        encoders = TransformerEncoder(encoder_layers, n_layers)
+        self.transformer_encoder = UniversalTransformer(encoders, n_layers)
 
         # initialize alphabet to random values between -pi and pi
         u = torch.distributions.Uniform(-3.14, 3.14)
@@ -207,9 +208,9 @@ class UniversalTransformer(nn.Module):
         # the remainders when it halted this step
         update_weights = p * still_running + new_halted * remainders
 
-        transformed_state = state
-        for i in range(self.n_layers):
-            transformed_state = self.encoder_layers(transformed_state, input_mask)
+        #transformed_state = state
+        # for i in range(self.n_layers):
+        transformed_state = self.encoder_layers(state, input_mask)
 
         transformed_state = transformed_state.transpose(0, 1)
         new_state = ((transformed_state * update_weights) + (previous_state *
