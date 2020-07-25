075556400785c49a252512df212378dab4655cc8
alifkurniawan <cs.alif.kurniawan@gmail.com>
2020-07-26 02:26:54 +0700

    float a

--- openprotein.py
+++ openprotein.py
@@ -142,6 +142,7 @@ class BaseModel(nn.Module):
 
     def evaluate_model(self, data_loader):
         loss = 0
+        angular_loss = 0
         data_total = []
         dRMSD_list = []
         RMSD_list = []
@@ -157,12 +158,24 @@ class BaseModel(nn.Module):
                                       cpu_predicted_angles,
                                       cpu_predicted_backbone_atoms))
             data_total.extend(minibatch_data)
+            actual_coords_list_padded, batch_sizes_coords = torch.nn.utils.rnn.pad_packed_sequence(
+                torch.nn.utils.rnn.pack_sequence(tertiary_positions))
+            if self.use_gpu:
+                actual_coords_list_padded = actual_coords_list_padded.cuda()
+            emissions_actual, _ = calculate_dihedral_angles_over_minibatch(actual_coords_list_padded,
+                                                                           batch_sizes_coords,
+                                                                           self.use_gpu)
+
             start = time.time()
             for primary_sequence, tertiary_positions, _predicted_pos, predicted_backbone_atoms \
                     in minibatch_data:
                 actual_coords = tertiary_positions.transpose(0, 1).contiguous().view(-1, 3)
                 predicted_coords = predicted_backbone_atoms[:len(primary_sequence)] \
                     .transpose(0, 1).contiguous().view(-1, 3).detach()
+
+                if self.use_gpu:
+                    emissions_actual = emissions_actual.cuda()
+                angular_loss += float(calc_angular_difference(predicted_angles, emissions_actual))
 
                 rmsd = calc_rmsd(predicted_coords, actual_coords)
                 drmsd = calc_drmsd(predicted_coords, actual_coords)
@@ -173,6 +186,7 @@ class BaseModel(nn.Module):
                 end = time.time()
             write_out("Calculate validation loss for minibatch took:", end - start)
         loss /= data_loader.dataset.__len__()
+        angular_loss /= data_loader.dataset.__len__()
         self.historical_rmsd_avg_values.append(float(torch.Tensor(RMSD_list).mean()))
         self.historical_drmsd_avg_values.append(float(torch.Tensor(dRMSD_list).mean()))
 
@@ -184,7 +198,7 @@ class BaseModel(nn.Module):
             pos_pred = pos_pred.cuda()
         angles = calculate_dihedral_angles(pos, self.use_gpu)
         angles_pred = calculate_dihedral_angles(pos_pred, self.use_gpu)
-        diff_angles = calc_angular_difference(angles_pred, angles)
+
         write_to_pdb(get_structure_from_angles(prim, angles), "test")
         write_to_pdb(get_structure_from_angles(prim, angles_pred), "test_pred")
 
@@ -200,4 +214,4 @@ class BaseModel(nn.Module):
 
         prediction_data = None
 
-        return loss, data, prediction_data, diff_angles
+        return loss, data, prediction_data, angular_loss
